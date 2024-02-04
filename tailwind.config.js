e6ef4fa248299991063daec4fd7e13e7e1574dd8
Hassnaedh <dehbihassnae@gmail.com>
2024-02-04 20:08:30 +0100

    social links profile challenge

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#c5f82a',
+        grey: '#333333',
+        dgrey: '#1f1f1f',
+        black: '#141414',
+
+      },
+      fontFamily: {
+         body: "Inter",
+      },
+    },
+  },
+  plugins: [],
+}
+
